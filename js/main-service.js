10f6710a306a239418d6ca41ba86ba70595ac1c1
Omer <omerbnb@gmail.com>
2023-02-20 00:10:40 +0200

    changes

--- js/main-service.js
+++ js/main-service.js
@@ -384,7 +384,6 @@ function FilterMemes(val) {
 
 
 function renderImg(img) {
-  // Draw the img on the canvas
   gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
   console.log('img',img.src)
   let newImg = {
